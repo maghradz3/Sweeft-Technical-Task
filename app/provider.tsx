7cd3d718d596d55e55fa0b75d3d42489563ebf06
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2024-03-01 14:00:23 +0400

    starting new project

---
+++ app/provider.tsx
@@ -0,0 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React, { useState } from "react";
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000 * 5,
+        },
+      },
+    });
+  });
+
+  return (
+    <>
+      <QueryClientProvider client={new QueryClient()}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
+};
+
+export default Providers;
